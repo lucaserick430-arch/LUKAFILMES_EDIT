7cb8b19f423a381162fd4f2559f1da39e16838e1
Lucas Erick <jadiiquelesantos@gmail.com>
2026-08-20 15:17:44 -0300

    Trocar bcrypt por bcryptjs no Render

--- server_render.js
+++ server_render.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 ﻿
 const express = require("express");
 const path = require("path");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const session = require("express-session");
 
 const { getStore } = require("@netlify/blobs");
