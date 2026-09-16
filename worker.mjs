8b6a5ff42555e9af564b6d17c1dc5d0e23c1fad8
lucaserick430-arch <lucaserick430-arch@users.noreply.github.com>
2026-09-16 16:16:34 -0300

    Corrigir entrada do Worker Cloudflare

--- worker.mjs
+++ worker.mjs
@@ -2,6 +2,6 @@ const { httpServerHandler } = require("cloudflare:node");
 
 const app = require("./server_render.js");
 
-module.exports = httpServerHandler({
-  port: 3000
-});
+app.listen(3000);
+
+export default httpServerHandler({ port: 3000 });
